40a3ef74337a59d9c307a6271216574700ffb6e6
salahufegno <swalahudheen.fegno@gmail.com>
2024-11-12 10:09:02 +0530

    gettter : context

--- apps/feggy_core/lib/src/core.dart
+++ apps/feggy_core/lib/src/core.dart
@@ -4,8 +4,15 @@ import 'package:feggy_core/imports_bindings.dart';
 import 'package:feggy_network/imports_bindings.dart';
 
 class Feggy {
+  ///
   static final GlobalKey<NavigatorState> navKey = GlobalKey<NavigatorState>();
 
+  ///
+  static BuildContext? get context {
+    return navKey.currentContext;
+  }
+
+  ///
   static Future<T> async<R, T>({
     required Future<R> call,
     required FutureOr<T> Function(R) onSuccess,
