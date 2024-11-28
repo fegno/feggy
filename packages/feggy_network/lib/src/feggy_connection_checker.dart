7a021882e5d505fe2a487dd34ccd269d60114dff
salahu01 <cvsalahu12@gmail.com>
2024-11-28 22:44:33 +0530

    fix: invert connection check logic in FeggyConnectionChecker

--- packages/feggy_network/lib/src/feggy_connection_checker.dart
+++ packages/feggy_network/lib/src/feggy_connection_checker.dart
@@ -2,10 +2,10 @@ import 'package:feggy_network/imports_bindings.dart';
 
 class FeggyConnectionChecker {
   static Future<bool> get hasConnection async {
-    return (await Connectivity().checkConnectivity()).contains(ConnectivityResult.none);
+    return !(await Connectivity().checkConnectivity()).contains(ConnectivityResult.none);
   }
 
   static Stream<bool> get onConnectionChanged {
-    return Connectivity().onConnectivityChanged.map((connections) => connections.contains(ConnectivityResult.none));
+    return Connectivity().onConnectivityChanged.map((connections) => !connections.contains(ConnectivityResult.none));
   }
 }
